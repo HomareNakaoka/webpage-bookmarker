69e591ef5d87a963cac9dd472dc4a4f059609253
HomareNakaoka <noname.practice@gmail.com>
2024-02-14 14:07:15 +0900

    キー押下で送信できるように

--- public/webpage-bookmarker-util.js
+++ public/webpage-bookmarker-util.js
@@ -4,3 +4,31 @@ const toolTipTriggerElements = document.querySelectorAll('[data-bs-toggle="toolt
 toolTipTriggerElements.forEach((toolTipTriggerElement) => {
   new bootstrap.Tooltip(toolTipTriggerElement);
 });
+
+const formElement = document.forms['content-form'];
+const contentTextareaElement = formElement.elements['content'];
+const noteTextareaElement = formElement.elements['note'];
+contentTextareaElement.addEventListener('keydown', (event) => {
+  if (isPressedSubmitKey(event)) {
+    event.preventDefault();
+    formElement.submit();
+  }
+});
+noteTextareaElement.addEventListener('keydown', (event) => {
+  if (isPressedSubmitKey(event)) {
+    event.preventDefault();
+    formElement.submit();
+  }
+})
+
+function isPressedSubmitKey(event) {
+  if (event.key !== 'Enter') {
+    return false;
+  }
+  if (event.ctrlKey) {
+    return true;
+  }
+  if (event.metaKey) {
+    return true;
+  }
+}
