745cc2facb1c67a06126a0ea2bb18c62fc37e3b0
HomareNakaoka <noname.practice@gmail.com>
2024-08-20 07:44:51 +0900

    認証前にトップページを表示するように

--- index.js
+++ index.js
@@ -2,15 +2,26 @@
 const http = require('node:http');
 const auth = require('http-auth');
 const router = require('./lib/router');
+const util = require('./lib/handler-util');
 
 const basic = auth.basic({
   realm: 'Enter username and password.',
   file: './users.htpasswd'
 });
 
-const server = http.createServer(basic.check((req, res) => {
-  router.route(req, res);
-})).on('error', e => {
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type':'text/charset=utf-8'
+    });
+    res.end(util.handleTop(req, res));
+  } else {
+    basic.check((req, res) => {
+      router.route(req, res);
+    })
+    (req, res);
+  }
+}).on('error', e => {
   console.error('Server Error', e);
 }).on('clientError', e => {
   console.error('Client Error', e);
